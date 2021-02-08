cd60757c55044405ab6af4d79a79c3315837f99b
Kjetil Øverby <kjetiloverby@imac.home>
2021-02-08 12:19:44 +0100

    First commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FooterComponent from '../components/commonComponents/FooterComponent';
+import StartPageComponent from '../components/startpageComponent/StartPageComponent';
+import StartPageSection from '../components/startpageComponent/StartPageSection';
+import StartPageSection2 from '../components/startpageComponent/StartPageSection2';
+import StartPageSection3 from '../components/startpageComponent/StartPageSection3';
+
+export default function Index() {
+  return (
+    <div>
+      <StartPageComponent />
+      <StartPageSection />
+      <StartPageSection2 />
+      <StartPageSection3 />
+      <FooterComponent />
+    </div>
+  );
+}
